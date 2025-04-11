59bd0deb1387544b5f3a8bc87ff8b90be9ee9c73
Emmanuel Anebi <anebiemmanuel@gmail.com>
2025-04-11 12:04:19 +0100

    refactor: update ALLOWED_HOSTS configuration to support multiple hosts in development and production settings

--- server/settings/environments/development.py
+++ server/settings/environments/development.py
@@ -11,16 +11,8 @@ import socket
 from typing import TYPE_CHECKING
 
 from server.settings.components import config
-from server.settings.components.common import (
-    DATABASES,
-    INSTALLED_APPS,
-    MIDDLEWARE,
-)
-from server.settings.components.csp import (
-    CSP_CONNECT_SRC,
-    CSP_IMG_SRC,
-    CSP_SCRIPT_SRC,
-)
+from server.settings.components.common import DATABASES, INSTALLED_APPS, MIDDLEWARE
+from server.settings.components.csp import CSP_CONNECT_SRC, CSP_IMG_SRC, CSP_SCRIPT_SRC
 
 if TYPE_CHECKING:
     from django.http import HttpRequest
@@ -29,12 +21,15 @@ if TYPE_CHECKING:
 
 DEBUG = True
 
+HOSTS = config("ALLOWED_HOSTS", default="")
+
 ALLOWED_HOSTS = [
-    config('DOMAIN_NAME'),
-    'localhost',
-    '0.0.0.0',  # noqa: S104
-    '127.0.0.1',
-    '[::1]',
+    # Split the domains by comma and filter out empty strings
+    *[host.strip() for host in HOSTS.split(",") if host.strip()],
+    "localhost",
+    "0.0.0.0",  # noqa: S104
+    "127.0.0.1",
+    "[::1]",
 ]
 
 
@@ -42,22 +37,22 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS += (
     # Better debug:
-    'debug_toolbar',
-    'nplusone.ext.django',
+    "debug_toolbar",
+    "nplusone.ext.django",
     # Linting migrations:
-    'django_migration_linter',
+    "django_migration_linter",
     # django-test-migrations:
-    'django_test_migrations.contrib.django_checks.AutoNames',
+    "django_test_migrations.contrib.django_checks.AutoNames",
     # This check might be useful in production as well,
     # so it might be a good idea to move `django-test-migrations`
     # to prod dependencies and use this check in the main `settings.py`.
     # This will check that your database is configured properly,
     # when you run `python manage.py check` before deploy.
-    'django_test_migrations.contrib.django_checks.DatabaseConfiguration',
+    "django_test_migrations.contrib.django_checks.DatabaseConfiguration",
     # django-extra-checks:
-    'extra_checks',
+    "extra_checks",
     # django-query-counter:
-    'query_counter',
+    "query_counter",
 )
 
 
@@ -65,21 +60,21 @@ INSTALLED_APPS += (
 # https://django-debug-toolbar.readthedocs.io
 
 MIDDLEWARE += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     # https://github.com/conformist-mw/django-query-counter
     # Prints how many queries were executed, useful for the APIs.
-    'query_counter.middleware.DjangoQueryCounterMiddleware',
+    "query_counter.middleware.DjangoQueryCounterMiddleware",
 )
 
 # https://django-debug-toolbar.readthedocs.io/en/stable/installation.html#configure-internal-ips
 try:  # This might fail on some OS
     INTERNAL_IPS = [
-        '{}.1'.format(ip[: ip.rfind('.')])
+        "{}.1".format(ip[: ip.rfind(".")])
         for ip in socket.gethostbyname_ex(socket.gethostname())[2]
     ]
 except OSError:  # pragma: no cover
     INTERNAL_IPS = []
-INTERNAL_IPS += ['127.0.0.1', '10.0.2.2']
+INTERNAL_IPS += ["127.0.0.1", "10.0.2.2"]
 
 
 def _custom_show_toolbar(request: HttpRequest) -> bool:
@@ -88,15 +83,15 @@ def _custom_show_toolbar(request: HttpRequest) -> bool:
 
 
 DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': (
-        'server.settings.environments.development._custom_show_toolbar'
+    "SHOW_TOOLBAR_CALLBACK": (
+        "server.settings.environments.development._custom_show_toolbar"
     ),
 }
 
 # This will make debug toolbar to work with django-csp,
 # since `ddt` loads some scripts from `ajax.googleapis.com`:
-CSP_SCRIPT_SRC += ('ajax.googleapis.com',)
-CSP_IMG_SRC += ('data:',)
+CSP_SCRIPT_SRC += ("ajax.googleapis.com",)
+CSP_IMG_SRC += ("data:",)
 CSP_CONNECT_SRC += ("'self'",)
 
 
@@ -104,14 +99,14 @@ CSP_CONNECT_SRC += ("'self'",)
 # https://github.com/jmcarp/nplusone
 
 # Should be the first in line:
-MIDDLEWARE = ('nplusone.ext.django.NPlusOneMiddleware', *MIDDLEWARE)
+MIDDLEWARE = ("nplusone.ext.django.NPlusOneMiddleware", *MIDDLEWARE)
 
 # Logging N+1 requests:
 NPLUSONE_RAISE = True  # comment out if you want to allow N+1 requests
-NPLUSONE_LOGGER = logging.getLogger('django')
+NPLUSONE_LOGGER = logging.getLogger("django")
 NPLUSONE_LOG_LEVEL = logging.WARNING
 NPLUSONE_WHITELIST = [
-    {'model': 'admin.*'},
+    {"model": "admin.*"},
 ]
 
 
@@ -119,16 +114,16 @@ NPLUSONE_WHITELIST = [
 # https://github.com/wemake-services/django-test-migrations
 
 # Set of badly named migrations to ignore:
-DTM_IGNORED_MIGRATIONS = frozenset((('axes', '*'),))
+DTM_IGNORED_MIGRATIONS = frozenset((("axes", "*"),))
 
 
 # django-migration-linter
 # https://github.com/3YOURMIND/django-migration-linter
 
 MIGRATION_LINTER_OPTIONS = {
-    'exclude_apps': ['axes'],
-    'exclude_migration_tests': ['CREATE_INDEX', 'CREATE_INDEX_EXCLUSIVE'],
-    'warnings_as_errors': True,
+    "exclude_apps": ["axes"],
+    "exclude_migration_tests": ["CREATE_INDEX", "CREATE_INDEX_EXCLUSIVE"],
+    "warnings_as_errors": True,
 }
 
 
@@ -136,29 +131,29 @@ MIGRATION_LINTER_OPTIONS = {
 # https://github.com/kalekseev/django-extra-checks
 
 EXTRA_CHECKS = {
-    'checks': [
+    "checks": [
         # Forbid `unique_together`:
-        'no-unique-together',
+        "no-unique-together",
         # Each model must be registered in admin:
-        'model-admin',
+        "model-admin",
         # FileField/ImageField must have non empty `upload_to` argument:
-        'field-file-upload-to',
+        "field-file-upload-to",
         # Text fields shouldn't use `null=True`:
-        'field-text-null',
+        "field-text-null",
         # Don't pass `null=False` to model fields (this is django default)
-        'field-null',
+        "field-null",
         # ForeignKey fields must specify db_index explicitly if used in
         # other indexes:
-        {'id': 'field-foreign-key-db-index', 'when': 'indexes'},
+        {"id": "field-foreign-key-db-index", "when": "indexes"},
         # If field nullable `(null=True)`,
         # then default=None argument is redundant and should be removed:
-        'field-default-null',
+        "field-default-null",
         # Fields with choices must have companion CheckConstraint
         # to enforce choices on database level
-        'field-choices-constraint',
+        "field-choices-constraint",
     ],
 }
 
 # Disable persistent DB connections
 # https://docs.djangoproject.com/en/4.2/ref/databases/#caveats
-DATABASES['default']['CONN_MAX_AGE'] = 0
+DATABASES["default"]["CONN_MAX_AGE"] = 0
