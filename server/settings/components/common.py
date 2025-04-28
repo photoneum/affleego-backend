5dfa385ef34399e403087e93e5e2b09b9521aa6b
Emmanuel Anebi <anebiemmanuel@gmail.com>
2025-04-28 05:50:06 +0100

    feat: add pre-commit configuration and update Django settings for new dependencies

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -15,86 +15,89 @@ from server.settings.components import BASE_DIR, config
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_KEY = config("DJANGO_SECRET_KEY")
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # Application definition:
 
 INSTALLED_APPS: tuple[str, ...] = (
     # Your apps go here:
-    "server.apps.main",
-    "server.apps.users",
+    'server.apps.main',
+    'server.apps.users',
     # Default django apps:
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     # django-admin:
-    "jazzmin",
-    "django.contrib.admin",
-    "django.contrib.admindocs",
+    'jazzmin',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
     # Security:
-    "axes",
+    'axes',
     # Health checks:
     # You may want to enable other checks as well,
     # see: https://github.com/KristianOellegaard/django-health-check
-    "health_check",
-    "health_check.db",
-    "health_check.cache",
-    "health_check.storage",
+    'health_check',
+    'health_check.db',
+    'health_check.cache',
+    'health_check.storage',
     # Rest framework:
-    "rest_framework",
+    'rest_framework',
     # "rest_framework_simplejwt",
     # "rest_framework_simplejwt.token_blacklist",
     # "rest_framework_simplejwt.token_blacklist.models",
     # "rest_framework_simplejwt.token_blacklist.views",
-    "drf_spectacular",
+    'drf_spectacular',
+    # corsheaders
+    'corsheaders',
 )
 
 MIDDLEWARE: tuple[str, ...] = (
     # Logging:
-    "server.settings.components.logging.LoggingContextVarsMiddleware",
+    'server.settings.components.logging.LoggingContextVarsMiddleware',
     # Content Security Policy:
-    "csp.middleware.CSPMiddleware",
+    'csp.middleware.CSPMiddleware',
     # Django:
-    "django.middleware.security.SecurityMiddleware",
+    'django.middleware.security.SecurityMiddleware',
     # django-permissions-policy
-    "django_permissions_policy.PermissionsPolicyMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django_permissions_policy.PermissionsPolicyMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # Axes:
-    "axes.middleware.AxesMiddleware",
+    'axes.middleware.AxesMiddleware',
 )
 
-ROOT_URLCONF = "server.urls"
+ROOT_URLCONF = 'server.urls'
 
 # Disable automatic trailing slash redirection
 # https://docs.djangoproject.com/en/4.2/ref/settings/#append-slash
 APPEND_SLASH = True
 
-WSGI_APPLICATION = "server.wsgi.application"
+WSGI_APPLICATION = 'server.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("POSTGRES_DB"),
-        "USER": config("POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES_PASSWORD"),
-        "HOST": config("DJANGO_DATABASE_HOST"),
-        "PORT": config("DJANGO_DATABASE_PORT", cast=int),
-        "CONN_MAX_AGE": config("CONN_MAX_AGE", cast=int, default=60),
-        "OPTIONS": {
-            "connect_timeout": 10,
-            "options": "-c statement_timeout=15000ms",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('DJANGO_DATABASE_HOST'),
+        'PORT': config('DJANGO_DATABASE_PORT', cast=int),
+        'CONN_MAX_AGE': config('CONN_MAX_AGE', cast=int, default=60),
+        'OPTIONS': {
+            'connect_timeout': 10,
+            'options': '-c statement_timeout=15000ms',
             # consider using 'isolation_level' set to 'serializable'
         },
     },
@@ -102,35 +105,35 @@ DATABASES = {
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'en-us'
 
 USE_I18N = True
 
 LANGUAGES = (
-    ("en", _("English")),
-    ("ru", _("Russian")),
+    ('en', _('English')),
+    ('ru', _('Russian')),
 )
 
-LOCALE_PATHS = ("locale/",)
+LOCALE_PATHS = ('locale/',)
 
 USE_TZ = True
-TIME_ZONE = "UTC"
+TIME_ZONE = 'UTC'
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = (
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
 
@@ -139,21 +142,21 @@ STATICFILES_FINDERS = (
 
 TEMPLATES = [
     {
-        "APP_DIRS": True,
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [
+        'APP_DIRS': True,
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
             # Contains plain text templates, like `robots.txt`:
-            BASE_DIR.joinpath("server", "common", "django", "templates"),
+            BASE_DIR.joinpath('server', 'common', 'django', 'templates'),
         ],
-        "OPTIONS": {
-            "context_processors": [
+        'OPTIONS': {
+            'context_processors': [
                 # Default template context processors:
-                "django.contrib.auth.context_processors.auth",
-                "django.template.context_processors.debug",
-                "django.template.context_processors.i18n",
-                "django.template.context_processors.media",
-                "django.contrib.messages.context_processors.messages",
-                "django.template.context_processors.request",
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     }
@@ -165,23 +168,23 @@ TEMPLATES = [
 # (see development.py and production.py).
 # https://docs.djangoproject.com/en/4.2/topics/files/
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR.joinpath("media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media')
 
 
 # Django authentication system
 # https://docs.djangoproject.com/en/4.2/topics/auth/
 
 AUTHENTICATION_BACKENDS = (
-    "axes.backends.AxesBackend",
-    "django.contrib.auth.backends.ModelBackend",
+    'axes.backends.AxesBackend',
+    'django.contrib.auth.backends.ModelBackend',
 )
 
 PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.Argon2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
 ]
 
 
@@ -193,11 +196,11 @@ CSRF_COOKIE_HTTPONLY = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 
-X_FRAME_OPTIONS = "DENY"
+X_FRAME_OPTIONS = 'DENY'
 
 # https://docs.djangoproject.com/en/3.0/ref/middleware/#referrer-policy
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-SECURE_REFERRER_POLICY = "same-origin"
+SECURE_REFERRER_POLICY = 'same-origin'
 
 # https://github.com/adamchainz/django-permissions-policy#setting
 PERMISSIONS_POLICY: dict[str, str | list[str]] = {}
@@ -211,110 +214,114 @@ EMAIL_TIMEOUT = 5
 # Jazzmin
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "Engine Sync Admin",
+    'site_title': 'Engine Sync Admin',
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "Engine Sync",
+    'site_header': 'Engine Sync',
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "Engine Sync",
+    'site_brand': 'Engine Sync',
     # Logo to use for your site, must be present in static files, used for brand on top left
     # "site_logo": "books/img/logo.png",
-    "site_logo": None,
+    'site_logo': None,
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": None,
+    'login_logo': None,
     # Logo to use for login form in dark themes (defaults to login_logo)
-    "login_logo_dark": None,
+    'login_logo_dark': None,
     # CSS classes that are applied to the logo above
-    "site_logo_classes": "img-circle",
+    'site_logo_classes': 'img-circle',
     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
-    "site_icon": None,
+    'site_icon': None,
     # Welcome text on the login screen
-    "welcome_sign": "Welcome to the Engine Sync",
+    'welcome_sign': 'Welcome to the Engine Sync',
     # Copyright on the footer
-    "copyright": "Engine Sync",
+    'copyright': 'Engine Sync',
     # List of model admins to search from the search bar, search bar omitted if excluded
     # If you want to use a single search field you dont need to use a list, you can use a simple string
-    "search_model": ["auth.User", "auth.Group"],
+    'search_model': ['auth.User', 'auth.Group'],
     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
-    "user_avatar": None,
+    'user_avatar': None,
     ############
     # Top Menu #
     ############
     # Links to put along the top menu
-    "topmenu_links": [
+    'topmenu_links': [
         # Url that gets reversed (Permissions can be added)
-        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+        {
+            'name': 'Home',
+            'url': 'admin:index',
+            'permissions': ['auth.view_user'],
+        },
         # external url that opens in a new window (Permissions can be added)
         {
-            "name": "Support",
-            "url": "https://github.com/farridav/django-jazzmin/issues",
-            "new_window": True,
+            'name': 'Support',
+            'url': 'https://github.com/farridav/django-jazzmin/issues',
+            'new_window': True,
         },
         # model admin to link to (Permissions checked against model)
-        {"model": "auth.User"},
+        {'model': 'auth.User'},
         # App with dropdown menu to all its models pages (Permissions checked against models)
-        {"app": "books"},
+        {'app': 'books'},
     ],
     #############
     # User Menu #
     #############
     # Additional links to include in the user menu on the top right ("app" url type is not allowed)
-    "usermenu_links": [
+    'usermenu_links': [
         {
-            "name": "Support",
-            "url": "https://github.com/farridav/django-jazzmin/issues",
-            "new_window": True,
+            'name': 'Support',
+            'url': 'https://github.com/farridav/django-jazzmin/issues',
+            'new_window': True,
         },
-        {"model": "auth.user"},
+        {'model': 'auth.user'},
     ],
     #############
     # Side Menu #
     #############
     # Whether to display the side menu
-    "show_sidebar": True,
+    'show_sidebar': True,
     # Whether to aut expand the menu
-    "navigation_expanded": True,
+    'navigation_expanded': True,
     # Hide these apps when generating side menu e.g (auth)
-    "hide_apps": [],
+    'hide_apps': [],
     # Hide these models when generating side menu (e.g auth.user)
-    "hide_models": [],
+    'hide_models': [],
     # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
-    "order_with_respect_to": ["auth", "books", "books.author", "books.book"],
+    'order_with_respect_to': ['auth', 'books', 'books.author', 'books.book'],
     # Custom links to append to app groups, keyed on app name
-    "custom_links": {
-        "books": [
+    'custom_links': {
+        'books': [
             {
-                "name": "Make Messages",
-                "url": "make_messages",
-                "icon": "fas fa-comments",
-                "permissions": ["books.view_book"],
+                'name': 'Make Messages',
+                'url': 'make_messages',
+                'icon': 'fas fa-comments',
+                'permissions': ['books.view_book'],
             }
         ]
     },
     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
     # for the full list of 5.13.0 free icon classes
-    "icons": {
-        "auth": "fas fa-users-cog",
-        "auth.user": "fas fa-user",
-        "auth.Group": "fas fa-users",
+    'icons': {
+        'auth': 'fas fa-users-cog',
+        'auth.user': 'fas fa-user',
+        'auth.Group': 'fas fa-users',
     },
     # Icons that are used when one is not manually specified
-    "default_icon_parents": "fas fa-chevron-circle-right",
-    "default_icon_children": "fas fa-circle",
+    'default_icon_parents': 'fas fa-chevron-circle-right',
+    'default_icon_children': 'fas fa-circle',
     #################
     # Related Modal #
     #################
     # Use modals instead of popups
-    "related_modal_active": False,
+    'related_modal_active': False,
     #############
     # UI Tweaks #
     #############
     # Relative paths to custom CSS/JS scripts (must be present in static files)
-    "custom_css": None,
-    "custom_js": None,
+    'custom_css': None,
+    'custom_js': None,
     # Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)
-    "use_google_fonts_cdn": True,
+    'use_google_fonts_cdn': True,
     # Whether to show the UI customizer on the sidebar
-    "show_ui_builder": False,
+    'show_ui_builder': False,
     ###############
     # Change view #
     ###############
@@ -324,36 +331,36 @@ JAZZMIN_SETTINGS = {
     # - vertical_tabs
     # - collapsible
     # - carousel
-    "changeform_format": "horizontal_tabs",
+    'changeform_format': 'horizontal_tabs',
     # override change forms on a per modeladmin basis
-    "changeform_format_overrides": {
-        "auth.user": "collapsible",
-        "auth.group": "vertical_tabs",
+    'changeform_format_overrides': {
+        'auth.user': 'collapsible',
+        'auth.group': 'vertical_tabs',
     },
 }
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "Engine Sync API",
-    "DESCRIPTION": "Engine Sync API",
-    "VERSION": "1.0.0",
-    "SERVE_INCLUDE_SCHEMA": False,
-    "COMPONENT_SPLIT_REQUEST": True,
-    "SERVE_PERMISSIONS": ["rest_framework.permissions.AllowAny"],
+    'TITLE': 'Engine Sync API',
+    'DESCRIPTION': 'Engine Sync API',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'COMPONENT_SPLIT_REQUEST': True,
+    'SERVE_PERMISSIONS': ['rest_framework.permissions.AllowAny'],
 }
 
 # Email
 # https://docs.djangoproject.com/en/4.2/topics/email/
 # -------------------------------------------------------------------------------
 DEFAULT_FROM_EMAIL = config(
-    "DEFAULT_FROM_EMAIL",
-    default="Engine Sync Notifications <noreply@enginesync.com>",
+    'DEFAULT_FROM_EMAIL',
+    default='Engine Sync Notifications <noreply@enginesync.com>',
 )
--- server/settings/environments/staging.py
+++ server/settings/environments/staging.py
@@ -16,13 +16,13 @@ from server.settings.components.csp import CSP_CONNECT_SRC
 
 DEBUG = False
 
-HOSTS = config("ALLOWED_HOSTS", default="")
+HOSTS = config('ALLOWED_HOSTS', default='')
 
 ALLOWED_HOSTS = [
     # Split the domains by comma and filter out empty strings
-    *[host.strip() for host in HOSTS.split(",") if host.strip()],
+    *[host.strip() for host in HOSTS.split(',') if host.strip()],
     # We need this value for `healthcheck` to work:
-    "localhost",
+    'localhost',
 ]
 
 
@@ -32,36 +32,36 @@ ALLOWED_HOSTS = [
 # This is a hack to allow a special flag to be used with `--dry-run`
 # to test things locally.
 _COLLECTSTATIC_DRYRUN = config(
-    "DJANGO_COLLECTSTATIC_DRYRUN",
+    'DJANGO_COLLECTSTATIC_DRYRUN',
     cast=bool,
     default=False,
 )
 # Adding STATIC_ROOT to collect static files via 'collectstatic':
-STATIC_ROOT = ".static" if _COLLECTSTATIC_DRYRUN else "/var/www/django/static"
+STATIC_ROOT = '.static' if _COLLECTSTATIC_DRYRUN else '/var/www/django/static'
 
 # TODO: convert to `STORAGES`
 STATICFILES_STORAGE = (
     # This is a string, not a tuple,
     # but it does not fit into 80 characters rule.
-    "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+    'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 )
 
 
 # Media files
 # https://docs.djangoproject.com/en/4.2/topics/files/
 
-MEDIA_ROOT = "/var/www/django/media"
+MEDIA_ROOT = '/var/www/django/media'
 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-_PASS = "django.contrib.auth.password_validation"  # noqa: S105
+_PASS = 'django.contrib.auth.password_validation'  # noqa: S105
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": f"{_PASS}.UserAttributeSimilarityValidator"},
-    {"NAME": f"{_PASS}.MinimumLengthValidator"},
-    {"NAME": f"{_PASS}.CommonPasswordValidator"},
-    {"NAME": f"{_PASS}.NumericPasswordValidator"},
+    {'NAME': f'{_PASS}.UserAttributeSimilarityValidator'},
+    {'NAME': f'{_PASS}.MinimumLengthValidator'},
+    {'NAME': f'{_PASS}.CommonPasswordValidator'},
+    {'NAME': f'{_PASS}.NumericPasswordValidator'},
 ]
 
 
@@ -72,11 +72,11 @@ SECURE_HSTS_SECONDS = 31536000  # the same as Caddy has
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 SECURE_REDIRECT_EXEMPT = [
     # This is required for healthcheck to work:
-    "^health/",
+    '^health/',
 ]
 
 SESSION_COOKIE_SECURE = True
@@ -85,17 +85,19 @@ CSRF_COOKIE_SECURE = True
 # CSP
 CSP_CONNECT_SRC += (
     "'self'",
-    config("DOMAIN_NAME", default=""),
+    config('DOMAIN_NAME', default=''),
 )
 
-SPECTACULAR_SETTINGS = SPECTACULAR_SETTINGS_BASE.copy()  # noqa: E0602
-SPECTACULAR_SETTINGS["SERVERS"] = [
-    {"url": config("DOMAIN_NAME", default=""), "description": "Staging server"},
+SPECTACULAR_SETTINGS = SPECTACULAR_SETTINGS_BASE.copy()
+SPECTACULAR_SETTINGS['SERVERS'] = [
+    {'url': config('DOMAIN_NAME', default=''), 'description': 'Staging server'},
 ]
 
 # Email
 # https://docs.djangoproject.com/en/4.2/topics/email/
 # -------------------------------------------------------------------------------
-EMAIL_HOST = config("EMAIL_HOST", default="")
+EMAIL_HOST = config('EMAIL_HOST', default='')
 EMAIL_PORT = 1025
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=False)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False)
+
+CORS_ALLOWED_ORIGIN_REGEXES = [r'^/api/.*$', config('DOMAIN_NAME', default='')]
